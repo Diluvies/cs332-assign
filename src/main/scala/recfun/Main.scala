e8958ba33f41acc2b7776c4dab3aedc2d7d9b5ec
Diluvies <nevermore@postech.ac.kr>
2024-09-11 21:55:25 +0900

    Impelemented recursive functions

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -14,15 +16,42 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def isBalanced(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty)
+        openCount == 0
+      else if (chars.head == '(')
+        isBalanced(chars.tail, openCount + 1)
+      else if (chars.head == ')')
+        (openCount > 0) && isBalanced(chars.tail, openCount - 1)
+      else
+        isBalanced(chars.tail, openCount)
+    }
+
+    // check each char to see if parenthesis, from beginning
+    isBalanced(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || coins.isEmpty)
+      0
+    else if (money == 0)
+      1
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
